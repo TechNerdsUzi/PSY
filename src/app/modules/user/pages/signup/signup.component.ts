5c7d786131276fc9ba6670463bfac716616dc06f
TechNerdsUzi <uzairkhatri57@gmail.com>
2021-01-11 18:39:40 +0500

    Integreated firebase on signup

--- src/app/modules/user/pages/signup/signup.component.ts
+++ src/app/modules/user/pages/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as $ from 'jquery';
+import { AuthService } from '../../services/auth.service';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -8,7 +10,23 @@ import * as $ from 'jquery';
 export class SignupComponent implements OnInit, AfterViewInit {
 
   isDoctor: boolean = false;
-  constructor() { }
+  signupForm: FormGroup = new FormGroup({
+    name: new FormControl(null, Validators.required),
+    email: new FormControl(null, Validators.compose([Validators.required, Validators.email])),
+    mobile: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required)
+  });
+  name: AbstractControl;
+  email: AbstractControl;
+  mobile: AbstractControl;
+  password: AbstractControl;
+  showError: boolean = false;
+  constructor(private authService: AuthService) {
+    this.name = this.signupForm.controls['name'];
+    this.email = this.signupForm.controls['email'];
+    this.mobile = this.signupForm.controls['mobile'];
+    this.password = this.signupForm.controls['password'];
+  }
 
   ngOnInit(): void {
   }
@@ -23,6 +41,23 @@ export class SignupComponent implements OnInit, AfterViewInit {
 
   doctorToggle() {
     this.isDoctor = this.isDoctor ? false : true;
+  }
+
+  loginWithFacebook() {
+    this.authService.signInWithFacebook();
+  }
+
+  loginWithGoogle() {
+    this.authService.signInWithGoogle();
+  }
+
+  signupFormSubmit(formData) {
+    this.showError = false;
+    if (this.signupForm.valid) {
+      this.authService.SignUpUser(formData.email, formData.password);
+    } else {
+      this.showError = true;
+    }
   }
 
 }
--- src/app/modules/user/pages/signup/signup.module.ts
+++ src/app/modules/user/pages/signup/signup.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { SignupRoutingModule } from './signup-routing.module';
 import { SignupComponent } from './signup.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [SignupComponent],
   imports: [
     CommonModule,
-    SignupRoutingModule
+    SignupRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SignupModule { }
